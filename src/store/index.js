6dafde1fadd683547882bb56b32badbf1a267c82
lalithcodelover <lalith.vlr@gmail.com>
2022-11-29 21:47:32 +0530

    integrating redux with react added

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "INCREMENTBY2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "DECREMENTBY2") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
+  return state;
+};
+const store = createStore(counterReducer);
+
+export default store;
